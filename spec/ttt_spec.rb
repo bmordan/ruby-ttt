5453812b9af2f8b8d0401d2cf47a4877bb30d6d0
bmordan <bernardmordan@gmail.com>
2019-08-12 09:14:34 +0100

    updated version (from video)

--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -1,58 +1,67 @@
-require_relative "../ttt"
+require_relative "../ttt.rb"
 
-describe "The Game" do
-  it "has a zero index board" do
+describe "The Game of Tic Tac Toe" do
+  it "has a board" do
     expect(newBoard).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8])
   end
 
-  it "will auto swap players from X to O" do
+  it "can figure the player out" do
     expect(nextPlayer(newBoard)).to eq("X")
   end
 
-  it "can make moves" do
-    expect(move(8, newBoard)).to eq([0, 1, 2, 3, 4, 5, 6, 7, "X"])
+  it "current player" do
+    expect(currentPlayer(newBoard)).to eq("O")
   end
 
-  it "it will alternate counters" do
-    board1 = move(0, newBoard)
-    board2 = move(1, board1)
-    expect(board1).to eq(["X", 1, 2, 3, 4, 5, 6, 7, 8])
-    expect(board2).to eq(["X", "O", 2, 3, 4, 5, 6, 7, 8])
+  it "you play by making moves on the board" do
+    board = move(8, newBoard)
+    expect(board).to eq([0, 1, 2, 3, 4, 5, 6, 7, "X"])
+    board2 = move(0, board)
+    expect(board2).to eq(["O", 1, 2, 3, 4, 5, 6, 7, "X"])
   end
 
-  it "only allows valid moves" do
+  it "you can only move if its valid" do
     expect(valid?(20, newBoard)).to be(false)
-    expect(valid?(-2, newBoard)).to be(false)
+    expect(valid?(-3, newBoard)).to be(false)
     expect(valid?(0, newBoard)).to be(true)
     board = move(0, newBoard)
     expect(valid?(0, board)).to be(false)
   end
 
-  it "can turn the board to a string" do
-    board = ["X", "O", 2, 3, 4, "X", 6, 7, 8]
-    expect(board_to_string(board)).to eq("XO---X---")
-  end
-
-  it "will check for a win" do
-    hasWon = ["X", "X", "X", 3, 4, 5, 6, "O", "O"]
+  it "should sometimes have a winner" do
     expect(won?(newBoard)).to be(false)
-    expect(won?(hasWon)).to be(true)
+    expect(won?(["X", "X", "X", 3, 4, 5, 6, "O", "O"])).to be(true)
   end
 
-  it "will check for draws" do
-    expect(draw?(["X", "X", "X", 3, 4, 5, 6, "O", "O"])).to be(false)
-    expect(draw?(["X", "X", "O", "O", "O", "X", "X", "O", "X"])).to be(true)
+  it "can turn the board to a string" do
+    expect(board_to_string(["X", "O", 2, 3, 4, 5, 6, 7, "X"])).to eq("XO------X")
   end
 
-  it "will end in a terminal state" do
-    allow(self).to receive(:gets).and_return("2", "6", "8", "5", "1", "4")
-    end_state = cli_game(newBoard)
-    expect(end_state).to eq("O wins")
+  it "sometimes ends in a draw" do
+    expect(draw?(newBoard)).to be(false)
+    expect(draw?(["X", "X", "O", "O", "O", "X", "X", "X", "O"]))
   end
 
-  it "will display the board nicely" do
-    board = ["X", "X", "X", 3, 4, 5, 6, "O", "O"]
-    displayed_board = "X|X|X\n-----\n-|-|-\n-----\n-|O|O"
-    expect(display(board)).to eq(displayed_board)
+  it "should end with X winning" do
+    allow(self).to receive(:gets).and_return("1 ", " 9", "2", "8", "3")
+    final = game(newBoard)
+    expect(final).to eq("X wins")
+  end
+
+  it "should end with O winning" do
+    allow(self).to receive(:gets).and_return("9", "1", "8", "2", "5", "3")
+    final = game(newBoard)
+    expect(final).to eq("O wins")
+  end
+
+  it "could end with a draw" do
+    allow(self).to receive(:gets).and_return("1", "3", "2", "4", "6", "5", "7", "9", "8")
+    final = game(newBoard)
+    expect(final).to eq("draw")
+  end
+
+  it "should be seen printed in the command line" do
+    board = ["X", "O", "X", 3, 4, 5, 6, 7, "O"]
+    expect(display(board)).to eq("X|O|X\n-----\n-|-|-\n-----\n-|-|O")
   end
 end
--- ttt.rb
+++ ttt.rb
@@ -3,41 +3,33 @@ def newBoard
 end
 
 def nextPlayer(board)
-  plays = board.filter { |i| i.is_a?(Integer) }.count % 2
-  plays == 0 ? "O" : "X"
+  board.filter { |i| i.is_a? Integer }.count.odd? ? "X" : "O"
 end
 
-def prevPlayer(board)
-  nextPlayer(board) == "O" ? "X" : "O"
+def currentPlayer(board)
+  nextPlayer(board) == "X" ? "O" : "X"
+end
+
+def move(index, board)
+  player = nextPlayer(board)
+  if valid?(index, board)
+    nextBoard = board.clone
+    nextBoard[index] = player
+    nextBoard
+  else
+    puts "invalid input"
+    board
+  end
 end
 
 def valid?(index, board)
   in_range = index.between?(0, 8)
-  not_taken = board[index].is_a? Integer
+  not_taken = board[index].is_a?(Integer)
   in_range && not_taken
 end
 
-def move(index, board)
-  counter = nextPlayer(board)
-  nextBoard = board.clone
-  nextBoard[index] = counter
-  nextBoard
-end
-
-def play(board)
-  input = gets.strip
-  if valid?(input, board)
-    move(input, board)
-  else
-    puts "invalid move, try again"
-    play(board)
-  end
-end
-
 def board_to_string(board)
-  board
-    .map { |i| i.is_a?(Integer) ? "-" : i }
-    .join("")
+  board.map { |i| i.is_a?(Integer) ? "-" : i }.join("")
 end
 
 def won?(board)
@@ -50,31 +42,36 @@ def won?(board)
     "..#..#..#",
     "#...#...#",
     "..#.#.#..",
-  ].map { |pattern| pattern.gsub("#", prevPlayer(board)) }
-   .any? { |pattern| Regexp.new(pattern).match(board_to_string(board)) }
+  ].map { |pattern| pattern.gsub("#", currentPlayer(board)) }
+    .any? { |pattern| Regexp.new(pattern).match(board_to_string(board)) }
 end
 
 def draw?(board)
-  !board.any? { |i| i.is_a?(Integer) }
+  board.filter { |i| i.is_a? Integer }.empty?
 end
 
-def cli_game(prev_board)
+def game(board)
   input = gets.chomp.strip
   index = input.to_i - 1
-  board = move(index, prev_board)
-  puts display board
-  if won?(board)
-    "#{prevPlayer(board)} wins"
-  elsif draw?(board)
+
+  nextBoard = move(index, board)
+  puts display(nextBoard)
+  if won?(nextBoard)
+    puts "#{currentPlayer(nextBoard)} wins"
+    "#{currentPlayer(nextBoard)} wins"
+  elsif draw?(nextBoard)
+    puts "draw"
     "draw"
   else
-    cli_game(board)
+    game(nextBoard)
   end
 end
 
 def display(board)
   board_to_string(board)
     .chars
-    .each_slice(3).to_a
-    .map { |row| row.join("|") }.join "\n-----\n"
+    .each_slice(3)
+    .to_a
+    .map { |row| row.join("|") }
+    .join("\n-----\n")
 end
